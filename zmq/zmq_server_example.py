debcc49ba0f89214137146950f05b5333f25cd72
francis.tj <francistj20@gmail.com>
2021-08-10 17:11:05 +0530

    Working testcode for zmq in DC (the client script)     the server script was runned from the Vostro host PC

--- zmq/zmq_server_example.py
+++ zmq/zmq_server_example.py
@@ -8,16 +8,20 @@ import time
 import zmq
 
 context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
+socket = context.socket(zmq.PUB)
+socket.bind("tcp://*:1234")
+
+i = 0
 
 while True:
     #  Wait for next request from client
-    message = socket.recv()
-    print("Received request: %s" % message)
+    #message = socket.recv()
+    #print("Received request: %s" % message)
 
     #  Do some 'work'
     time.sleep(1)
 
     #  Send reply back to client
-    socket.send(b"World")
+    print("Sending %d"% i)
+    socket.send_string(str(i))
+    i += 1
